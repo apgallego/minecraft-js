b360ec17767f108974b1a1683c49c9f8193ec5e1
apgallego <apgalle03@gmail.com>
2025-10-22 00:52:43 +0200

    added controls + cube shaders

--- src/main.js
+++ src/main.js
@@ -1,33 +1,55 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-//setup renderer
+/**
+ * Renderer setup
+ */
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-//camera setup
+/**
+ * Camera setup
+ */
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight);
 camera.position.set(2, 2, 2);
 camera.lookAt(0, 0, 0);
 
-//scene setup
+//camera controls setup
+const controls = new OrbitControls(camera, renderer.domElement);
+
+/**
+ * Scene setup
+ */
 const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-// //add light
-// const light = new THREE.DirectionalLight(0xffffff, 1);
-// light.position.set(5, 10, 7.5);
-// scene.add(light);
+//add light
+const setupLights = () => {
+    const l1 = new THREE.DirectionalLight();
+    l1.position.set(1, 1, 1);
+    scene.add(l1);
 
-//render loop
+    const l2 = new THREE.DirectionalLight();
+    l2.position.set(-1, 1, -0.5);
+    scene.add(l2);
+
+    const ambient = new THREE.AmbientLight();
+    ambient.intensity = 0.1;
+    scene.add(ambient);
+};
+
+/**
+ * Render loop
+ */
 const animate = () => {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
     // cube.rotation.z += 0.01;
     renderer.render(scene, camera);
 };
@@ -37,4 +59,6 @@ window.addEventListener('resize', () => {
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
+
+setupLights();
 animate();
